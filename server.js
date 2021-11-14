97f2f685a6cb9417c3a0148e644240c06491887c
raizadattn <abhimanyu.raizada@tothenew.com>
2021-11-14 12:05:13 +0530

    added port and gitignore

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const http = require('http')
 
 const server = http.createServer((req,res)=>{
 })
-
-server.listen(3000, ()=>{
-    console.log(`Listening on port 3000`)
+const port = process.env.PORT || 3000
+server.listen(port, ()=>{
+    console.log(`Listening on port ${port}`)
 })
 
 
